15c1b44dd29d89d51ae6d016d0209303e3379246
Adham Mohamed <125216792+adham192@users.noreply.github.com>
2026-04-09 17:53:04 +0200

    Update app.py

--- Application/app.py
+++ Application/app.py
@@ -242,14 +242,13 @@ st.markdown("</div>", unsafe_allow_html=True)
 @st.cache_resource
 def load_model_and_stats():
     model_path = "bone_age_InceptionV3_final_8_8_MAE.keras"
-    
+    confirm_url = "https://drive.google.com/uc?export=download&id=1amkLQokqDAWR47tlfW66Hqlf_hjjHuhD&confirm=t"
     if not os.path.exists(model_path):
         with st.spinner("Loading model for the first time, please wait..."):
             gdown.download(
-                "https://drive.google.com/uc?id=1amkLQokqDAWR47tlfW66Hqlf_hjjHuhD",
+                confirm_url,
                 model_path,
-                quiet = False,
-                fuzzy = True
+                quiet = False
             )
     
     model = tf.keras.models.load_model(model_path, compile=False)
